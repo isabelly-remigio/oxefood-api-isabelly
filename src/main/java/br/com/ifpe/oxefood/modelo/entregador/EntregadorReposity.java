04a35c15bdf22654378f38e09d123c38a61ab6c8
isabelly-remigio <isabelly.remigio@gmail.com>
2025-09-18 15:56:18 -0300

    implementando o back do entregador

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorReposity.java
@@ -0,0 +1,7 @@
+package br.com.ifpe.oxefood.modelo.entregador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregadorReposity extends JpaRepository<Entregador, Long> {
+    
+}
